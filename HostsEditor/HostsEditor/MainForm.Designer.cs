5545063b35b5cc865c4e8b57fd356d4f500f80d4
BBitar <68905755+BBitarX@users.noreply.github.com>
2023-03-18 12:47:23 +0200

    Add files via upload

---
+++ HostsEditor/HostsEditor/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace HostsEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addIPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeIPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbServerIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLeave = new System.Windows.Forms.Button();
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(394, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.helpToolStripMenuItem.ForeColor = System.Drawing.Color.Gainsboro;
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addIPToolStripMenuItem,
+            this.removeIPToolStripMenuItem});
+            this.toolsToolStripMenuItem.ForeColor = System.Drawing.Color.Gainsboro;
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // addIPToolStripMenuItem
+            // 
+            this.addIPToolStripMenuItem.BackColor = System.Drawing.Color.Black;
+            this.addIPToolStripMenuItem.ForeColor = System.Drawing.Color.Gainsboro;
+            this.addIPToolStripMenuItem.Name = "addIPToolStripMenuItem";
+            this.addIPToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addIPToolStripMenuItem.Text = "Add IP";
+            this.addIPToolStripMenuItem.Click += new System.EventHandler(this.addIPToolStripMenuItem_Click);
+            // 
+            // removeIPToolStripMenuItem
+            // 
+            this.removeIPToolStripMenuItem.BackColor = System.Drawing.Color.Black;
+            this.removeIPToolStripMenuItem.ForeColor = System.Drawing.Color.Gainsboro;
+            this.removeIPToolStripMenuItem.Name = "removeIPToolStripMenuItem";
+            this.removeIPToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.removeIPToolStripMenuItem.Text = "Remove IP";
+            this.removeIPToolStripMenuItem.Click += new System.EventHandler(this.removeIPToolStripMenuItem_Click);
+            // 
+            // tbServerIP
+            // 
+            this.tbServerIP.Location = new System.Drawing.Point(193, 66);
+            this.tbServerIP.Name = "tbServerIP";
+            this.tbServerIP.Size = new System.Drawing.Size(172, 20);
+            this.tbServerIP.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(27, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server IP (IP Only):";
+            // 
+            // btnLeave
+            // 
+            this.btnLeave.BackColor = System.Drawing.Color.Transparent;
+            this.btnLeave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLeave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeave.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnLeave.Location = new System.Drawing.Point(205, 107);
+            this.btnLeave.Name = "btnLeave";
+            this.btnLeave.Size = new System.Drawing.Size(160, 23);
+            this.btnLeave.TabIndex = 3;
+            this.btnLeave.Text = "Remove IP (Leave)";
+            this.btnLeave.UseVisualStyleBackColor = false;
+            this.btnLeave.Click += new System.EventHandler(this.btnLeave_Click);
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.BackColor = System.Drawing.Color.Transparent;
+            this.btnJoin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnJoin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJoin.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnJoin.Location = new System.Drawing.Point(30, 107);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(160, 23);
+            this.btnJoin.TabIndex = 4;
+            this.btnJoin.Text = "Add IP (Join)";
+            this.btnJoin.UseVisualStyleBackColor = false;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 192);
+            this.Controls.Add(this.btnJoin);
+            this.Controls.Add(this.btnLeave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbServerIP);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "BBitar GTPS Joiner";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addIPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeIPToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbServerIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLeave;
+        private System.Windows.Forms.Button btnJoin;
+    }
+}
+
---
+++ HostsEditor/HostsEditor/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Created by BBitar#3731
+//Feel free to do wtv u want with the code/improve
+//Enjoy :D
+
+
+namespace HostsEditor
+{
+    public partial class MainForm : Form
+    {
+        
+        public MainForm()
+        {
+            
+            InitializeComponent();
+            SetBackgroundImage();
+            
+        }
+
+
+        private void SetBackgroundImage()
+        {
+            // Load the background image from Resources
+            Image backgroundImage = Properties.Resources.gamingImage;
+
+            this.BackgroundImage = backgroundImage;
+
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
+
+            
+            Image backgroundImage = this.BackgroundImage;
+
+            if (backgroundImage != null)
+            {
+                Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+
+                // Draw the background image behind all the controls
+                e.Graphics.DrawImage(backgroundImage, rect);
+            }
+        }
+        private void addIPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnJoin.PerformClick();
+        }
+
+        private void removeIPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnLeave.PerformClick();
+        }
+
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            // Get the server IP from the tbServerIP 
+            string serverIP = tbServerIP.Text;
+            AddHostsEntries(serverIP);
+            MessageBox.Show("Entries added to hosts file.", "BBitar GTPS Joiner", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+        private void btnLeave_Click(object sender, EventArgs e)
+        {
+            // Get the server IP from the tbServerIP 
+            string serverIP = tbServerIP.Text;
+            RemoveHostsEntries(serverIP);
+            MessageBox.Show("Entries removed from hosts file." ,"BBitar GTPS Joiner", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void AddHostsEntries(string serverIP)
+        {
+            
+            string[] entriesToAdd = new string[]
+            {
+        $"\n{serverIP} growtopia1.com",
+        $"{serverIP} growtopia2.com",
+        $"{serverIP} www.growtopia1.com",
+        $"{serverIP} www.growtopia2.com"
+            };
+
+            string hostsContent = File.ReadAllText(@"C:\Windows\System32\drivers\etc\hosts");
+            // Check if the entries already exist in the hosts file
+            bool entriesAlreadyExist = entriesToAdd.All(entry => hostsContent.Contains(entry));
+            if (!entriesAlreadyExist)
+            {
+                hostsContent += string.Join("\n", entriesToAdd) + "\n";
+                File.WriteAllText(@"C:\Windows\System32\drivers\etc\hosts", hostsContent);
+            }
+        }
+
+        private void RemoveHostsEntries(string serverIP)
+        {
+            string[] entriesToRemove = new string[]
+            {
+        $"{serverIP} growtopia1.com",
+        $"{serverIP} growtopia2.com",
+        $"{serverIP} www.growtopia1.com",
+        $"{serverIP} www.growtopia2.com"
+            };
+
+            // Get the contents of hosts file
+            string hostsContent = File.ReadAllText(@"C:\Windows\System32\drivers\etc\hosts");
+
+            // Remove entries from the hosts file
+            foreach (string entry in entriesToRemove)
+            {
+                hostsContent = hostsContent.Replace(entry + "\n", "");
+            }
+
+            // Write the modified hosts file contents back to the filepath
+            File.WriteAllText(@"C:\Windows\System32\drivers\etc\hosts", hostsContent);
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/BBitarX");
+        }
+    }
+}
+
